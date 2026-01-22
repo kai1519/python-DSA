f5c60f7f9e95c3732f50c8475a6de058e0324e92
kai1519 <prantikpriyam18@gmail.com>
2026-01-23 00:09:08 +0530

    add code for assignment

---
+++ assignment 15[list].py
@@ -0,0 +1,26 @@
+S1 = "prantik"
+S2 = """prantik"""
+S3 = str('prantik')
+print(S1,S2,S3,sep='\n')
+S1 = "TasinCoder"
+print(S1[ : 5 :])
+S1 = "Hello Learners"
+print(S1[2 : 5 :])
+a = ["Learning","Python"]
+print(' '.join(a))
+string="TasinCoder"
+print('Length is',len(string))
+S1="TasinCoder"
+print(S1[ : :-1])
+s1 = "Hi, I am coder"
+s2 = "coder"
+print(s2 in s1)
+S1="1234"
+print(S1.isdecimal())
+S1="TasinCoder"
+print(S1.isalpha())
+x=1234
+x=str(x)
+print(type(x))
+print(x)
+
